7657c09f427308d773ad95f3e3a1d553cbf06ef4
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-02-13 18:27:26 -0300

    test: testing references

---
+++ src/TesteReferencias.java
@@ -0,0 +1,28 @@
+
+public class TesteReferencias {
+
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 300;
+		
+		System.out.println("Saldo da primeira: " + primeiraConta.saldo);
+		
+		Conta segundaConta = primeiraConta;
+		
+		System.out.println("Saldo da segunda: " + segundaConta.saldo);
+		
+		segundaConta.saldo += 100;
+		System.out.println("Saldo da segunda: " + segundaConta.saldo);
+		
+		System.out.println("Saldo da primeira: " + primeiraConta.saldo);
+		
+		if(primeiraConta == segundaConta) {
+			System.out.println("São a mesma conta!");
+			
+			System.out.println(primeiraConta);
+			System.out.println(segundaConta);
+		}
+	}
+}
+
+// primeiraConta e segundaConta são referências para o mesmo objeto "Conta";
