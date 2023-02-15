acfb9525944df56d23bc5e6711c2660a64b18643
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-02-15 16:19:36 -0300

    feat: new method transfere

--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -2,12 +2,23 @@
 public class TestaMetodo {
 
 	public static void main(String[] args) {
-		Conta conta = new Conta();
-		conta.saldo = 100;
-		conta.deposita(50);
-		System.out.println(conta.saldo);
-		boolean conseguiuRetirar = conta.saca(20);
-		System.out.println(conta.saldo);
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.saldo = 100;
+		contaDoPaulo.deposita(50);
+		System.out.println(contaDoPaulo.saldo);
+		boolean conseguiuRetirar = contaDoPaulo.saca(20);
+		System.out.println(contaDoPaulo.saldo);
 		System.out.println(conseguiuRetirar);
+		
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		
+		if(contaDaMarcela.transfere(3000, contaDoPaulo)) {
+			System.out.println("Transferência com sucesso");
+		} else {
+			System.out.println("Faltou dinheiro");
+		}
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(contaDoPaulo.saldo);
 	}
 }
